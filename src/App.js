42ddba97c016c20ec4d43e89e3565106c30fec91
Jak <jaksawesome10@gmail.com>
2020-06-17 16:17:46 -0400

    basic stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <title>React Demo</title>
+      </header>
+
+      <main>
+        <h1>React Demo</h1>
+      </main>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
