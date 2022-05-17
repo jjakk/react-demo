2700999313d8d6d88fa534a083e4a0b8e8f6b4f2
John Khachian <jaksawesome10@gmail.com>
2022-05-17 19:31:04 -0400

    Updates

---
+++ precache-manifest.86742c12c5d851e7e69f0dc905cdab20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a0c777b3055c7b69e81177d48cd91e",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "3c1f31ceea6de6067acb",
+    "url": "/react-recipe-app/static/css/main.9d34dfe6.chunk.css"
+  },
+  {
+    "revision": "a9538a0c68710aa2539f",
+    "url": "/react-recipe-app/static/js/2.67933f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.67933f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1f31ceea6de6067acb",
+    "url": "/react-recipe-app/static/js/main.1ea42eae.chunk.js"
+  },
+  {
+    "revision": "4190ee9ea33bb94b6993",
+    "url": "/react-recipe-app/static/js/runtime-main.c68e6918.js"
+  }
+]);
